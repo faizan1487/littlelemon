2de5e37c61ab0aab68126e55d092b24499f052ca
faizan1487 <faizanahmed14887@gmail.com>
2023-05-11 17:02:21 +0500

    initial project

---
+++ lemonlistapi/views.py
@@ -0,0 +1,10 @@
+from django.shortcuts import render
+from rest_framework.response import Response
+from rest_framework import status
+from rest_framework.decorators import api_view
+from django.http import HttpResponse
+# Create your views here.
+
+@api_view()
+def lemonslist(request):
+    return Response('list of lemons', status=status.HTTP_200_OK)
